1964105d5a3a7dd7642cc292b05b2718a0a9ccc0
Poolevev <pulevgenii@gmail.com>
2023-04-10 15:40:08 +0300

    main structure created, set routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navigation from './components/navBar/Navigation';
+import Home from './components/homePage/Home';
+import VideoPage from './components/videoPlayerPage/VideoPage';
+import LibraryPage from './components/libraryPage/LibraryPage';
+import PlaylistsPage from './components/playlistsPage/PlaylistsPage';
+import LoginPage from './components/loginPage/LoginPage';
+import RegisterPage from './components/registration/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Box>
+      <Navigation />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/video/:id' element={<VideoPage />} />
+        <Route path='/library' element={<LibraryPage />} />
+        <Route path='/playlists' element={<PlaylistsPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='*' element={<box>Page not found</box>} />
+
+      </Routes>
+    </Box>
+
   );
 }
 
