de8d652509ce727c8bf47fb5df8b742fc06e4b9c
979969 <979969@gmail.com>
2023-04-13 21:26:27 +0300

    sidebar icons, sidebar position, logo, logo color effects

--- src/components/sideBar/Sidebar.js
+++ src/components/sideBar/Sidebar.js
@@ -2,18 +2,24 @@ import React from "react";
 import styles from "./Sidebar.module.scss";
 
 import { Link } from "react-router-dom";
+import homeLogo from "../../img/HomeLogo.png";
+import shortsLogo from "../../img/ShortsLogo.png";
+import libraryLogo from "../../img/LibraryLogo.webp";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <ul className={styles.linkList}>
         <li className={styles.linkItem}>
+          <img src={homeLogo} alt="home Logo" height={45} />
           <Link to="/">Home</Link>
         </li>
         <li className={styles.linkItem}>
+          <img src={shortsLogo} alt="shorts Logo" height={45} />
           <Link to="/">Shorts</Link>
         </li>
         <li className={styles.linkItem}>
+          <img src={libraryLogo} alt="library Logo" height={45} />
           <Link to="/library">Library</Link>
         </li>
       </ul>
