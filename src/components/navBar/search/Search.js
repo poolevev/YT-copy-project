a9cacc49b7ef93e0218df8b70b65b0f5e5f9736a
Poolevev <pulevgenii@gmail.com>
2023-04-19 16:41:05 +0300

    added voice search, made search bar stilisation

--- src/components/navBar/search/Search.js
+++ src/components/navBar/search/Search.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./SearchBar.module.scss";
+import { BsMic, BsXLg, BsSearch } from "react-icons/bs"
+
 
 const SearchBar = () => {
   const [searchedVideos, setSearchedVideo] = useState("");
+  const [isClearIcon, setIsClearIcon] = useState(true);
   const navigate = useNavigate();
 
-  const onhandleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     if (searchedVideos) {
@@ -14,7 +17,7 @@ const SearchBar = () => {
     }
   };
 
-  const onhandleClear = (event) => {
+  const handleClear = (event) => {
     event.preventDefault();
 
     if (searchedVideos) {
@@ -22,19 +25,33 @@ const SearchBar = () => {
     }
   };
 
+  const handleVoiceSearch = () => {
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.onresult = (event) => {
+      const transcript = event.results[0][0].transcript;
+      setSearchedVideo(transcript);
+      navigate(`/search/${transcript}`);
+    };
+    recognition.start();
+  };
+
   return (
-    <form className={styles.form} onSubmit={onhandleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
         className={styles.input}
         placeholder="Search..."
         value={searchedVideos}
         onChange={(e) => setSearchedVideo(e.target.value)}
       />
-      <button onClick={onhandleClear} className={styles.button} aria-label="search">
-        Clear
+      <button onClick={handleVoiceSearch} className={styles.buttonMic} aria-label="search">
+        <BsMic />
       </button>
-      <button className={styles.button} type="submit" aria-label="search">
-        Search
+      {isClearIcon && <button onClick={handleClear} className={styles.buttonClear} aria-label="search">
+        <BsXLg />
+      </button>}
+      <button className={styles.buttonSrc} type="submit" aria-label="search">
+        <BsSearch />
       </button>
     </form>
   );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 //import './reset.css';
 import { BrowserRouter } from 'react-router-dom';
-import {store} from './store/store'
-import { Provider } from 'react-redux'
+import {store} from './store/store';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
